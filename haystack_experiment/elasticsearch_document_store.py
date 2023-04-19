2e7025493c4b0c28a59933b72d4b4bf14c3d197b
Subkhon Amrulloev <amrulloev.suhon@gmail.com>
2023-04-19 20:41:17 +0900

    dolly viewer

--- haystack_experiment/elasticsearch_document_store.py
+++ haystack_experiment/elasticsearch_document_store.py
@@ -1,15 +1,22 @@
-import sys
-import pathlib
-sys.path.append(pathlib.Path.cwd())
-
 from haystack.document_stores.elasticsearch import ElasticsearchDocumentStore
 from load_subjqa_dataset import get_subjqa_dfs
 
-document_store=ElasticsearchDocumentStore(return_embedding=True)
-dfs=get_subjqa_dfs()
+from datasets import load_dataset
+import pandas
 
-for split,df in dfs.items():
-    docs=[{"text":row['context'],"meta":{"item_id":row['title'],"question_id":row['id'],"split":split}} for _,row in df.drop_duplicates(subset="context").iterrows()]
-    document_store.write_documents(docs,index="document")
+
+def get_subjqa_dfs():
+    subjqa = load_dataset("subjqa", name="electronics")
+    dfs = {split: dset.to_pandas() for split, dset in subjqa.flatten().items()}
+    return dfs
+
+
+document_store = ElasticsearchDocumentStore(return_embedding=True)
+dfs = get_subjqa_dfs()
+
+for split, df in dfs.items():
+    docs = [{"content": row['context'], "meta":{"item_id": row['title'], "question_id":row['id'],
+                                             "split":split}} for _, row in df.drop_duplicates(subset="context").iterrows()]
+    document_store.write_documents(docs, index="document")
 
 print(f"Loaded {document_store.get_document_count()} documents")
---
+++ haystack_experiment/elasticsearch_manual_install.md
@@ -0,0 +1,4 @@
+url ="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-linux-x86_64.tar.gz"
+
+wget -nc -q url
+tar -xzf filename
